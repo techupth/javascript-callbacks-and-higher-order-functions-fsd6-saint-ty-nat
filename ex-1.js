89097fda529c5b80715aaabb9a2a7e37fee4a1ad
Kittanut Thammapibaljit <tkittanut@gmail.com>
2024-05-17 11:26:02 +0700

    finished ex 1-2.

--- ex-1.js
+++ ex-1.js
@@ -2,11 +2,21 @@
 
 function forEach(array, operation) {
   // Start coding here
+  for (let i = 0; i < array.length; i++) {
+    let newSalary = increase5kSalary(array[i]);
+    newEmployeeSalaries.push(newSalary);
+  }
+}
+
+function increase5kSalary(num) {
+  let newNum = num + 5000;
+  return newNum;
 }
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
 // Using `forEach` function here
+forEach(employeeSalaries, increase5kSalary);
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,36 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let counter = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (operation(array[i]) === true) {
+      counter += 1;
+    }
+  }
+
+  let roomNum;
+
+  if (array === studentScoresRoom1) {
+    roomNum = 1;
+  } else if (array === studentScoresRoom2) {
+    roomNum = 2;
+  } else if (array === studentScoresRoom3) {
+    roomNum = 3;
+  }
+
+  if (counter >= 5) {
+    console.log(`นักเรียนห้องที่ ${roomNum} ผ่านเกณฑ์ ✅`);
+  } else {
+    console.log(`นักเรียนห้องที่ ${roomNum} ไม่ผ่านเกณฑ์ ❌`);
+  }
+}
+
+function checkScore(score) {
+  if (score > 70) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -13,3 +43,7 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 let scoreRoom1Result;
 let scoreRoom2Result;
 let scoreRoom3Result;
+
+scoreRoom1Result = atLeastFive(studentScoresRoom1, checkScore);
+scoreRoom2Result = atLeastFive(studentScoresRoom2, checkScore);
+scoreRoom3Result = atLeastFive(studentScoresRoom3, checkScore);
